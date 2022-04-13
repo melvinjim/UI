4480d8053e88fbb0e118afb57e004956497acd82
melvinjim <melvinjimenezang@hotmail.com>
2022-04-13 12:00:11 -0500

    add new title edit

--- js/edit_task.js
+++ js/edit_task.js
@@ -7,6 +7,7 @@ editTask = function(element){
 
     if(style.display == 'block'){
         table.style = 'display: none';
+
         if(styleTask.display == 'none'){
             task.style = 'display: block';
         };
@@ -14,6 +15,7 @@ editTask = function(element){
 
     if(createStyle.display == 'flex'){
         document.getElementById('create-task').style.display = 'none';
+
         if(editStyle.display == 'none'){
             document.getElementById('edit-task').style.display = 'block';
         };
@@ -33,29 +35,26 @@ editTask = function(element){
 }
 
 updateTask = function(){
-    row = element.parentNode.parentNode.parentNode;
-    tasks = row.getElementsByClassName("row-info");
-
-    function setTaskValues(title, limit, description){
+    function setTaskValues(title, limitDate, description){
         this.title = title;
-        this.limit = limit;
+        this.limitDate = limitDate;
         this.description = description;
     };
 
     var titleCapture = document.getElementById("title").value;
     var limitCapture = document.getElementById("limit-date").value;
+    var descriptionCapture = document.getElementById("description").value;
 
-    if(descriptionCapture != ""){
-        var descriptionCapture = document.getElementById("description").value;
-    }
-    
     newDate = new setTaskValues(titleCapture, limitCapture, descriptionCapture);
 
     if(styleTask.display == 'block'){
+        newTitleCapture= titleCapture;
+        newDate.title = newTitleCapture; 
+        document.getElementsByClassName("newdata").value = newTitleCapture;
         task.style = 'display: none';
 
         if(style.display == 'none'){
             table.style = 'display: block';
         }
     };
-}
+};
