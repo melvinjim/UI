4b47d74b7ddc1eb4c2731a7068790849260fb676
melvinjim <melvinjimenezang@hotmail.com>
2022-04-20 10:13:38 -0500

    hide table row at the moment the task is selected as completed

--- js/create_task.js
+++ js/create_task.js
@@ -17,6 +17,7 @@ var addTask = function(){
         if(styleTask.display == 'none'){
             document.getElementById("title-newtask").innerHTML =  "New Task"
             task.style = 'display: block'
+            document.getElementById("title").focus();
         }
     } else {
         table.style = 'display: block'
@@ -61,14 +62,18 @@ var createTask = function(){
     document.getElementById("limit-date").value = "";
 }
 
+var count = 0; 
+
 function createRow(){
     if (styleTask.display == 'block'){
-        document.getElementById("tbody-content").innerHTML += '<tr class="datos"> <td> <input type="checkbox" id="#" name="#" value="#" class="checkbox icon"> <label for="#"> </label> </td> <td class="row-info" data-title="' + newData.title + '" data-limit-date="' + newData.limitDate + '" data-description="' + newData.description + '"> <span class="newdata">' + newData.title + '</span></td><td></td><td ><a href="#" class="edit"> <i class="icons-edit fa-solid fa-pencil" onclick="editTask(this)"> </i> </a> <a href="#" data-bs-toggle="modal" data-bs-target="#exampleModal"> <i class="icons-trash fa-solid fa-trash-can" onclick="deleteBtn(this)"> </i> </a> </td> </tr>';
+        document.getElementById("tbody-content").innerHTML += '<tr class="datos"> <td> <input type="checkbox" id="checkbox-' + count + '" value="#" class="checkbox icon"> <label for="checkbox-' + count + '" onclick="completeTask(this)"> </label> </td> <td class="row-info" data-title="' + newData.title + '" data-limit-date="' + newData.limitDate + '" data-description="' + newData.description + '"> <span class="newdata">' + newData.title + '</span></td><td></td><td ><a href="#" class="edit"> <i class="icons-edit fa-solid fa-pencil" onclick="editTask(this)"> </i> </a> <a href="#" data-bs-toggle="modal" data-bs-target="#exampleModal"> <i class="icons-trash fa-solid fa-trash-can" onclick="deleteBtn(this)"> </i> </a> </td> </tr>';
 
         task.style = 'display: none';
 
         if(style.display == 'none'){
             table.style = 'display: block';
         }
+
+        count++
     }
 }
