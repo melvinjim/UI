90e49af4b9988ac5180cc75d82857108b5f6cb03
melvinjim <melvinjimenezang@hotmail.com>
2022-04-21 14:14:10 -0500

    dynamic date correction

--- js/dynamic_counter.js
+++ js/dynamic_counter.js
@@ -1,9 +1,9 @@
 var date = new Date();
 
-    formatDate = (date)=>{
-        var monthNameShort = date.toLocaleString("en-US", { month: "short" });
-        var formatted_date = date.getDay() + " " + + date.getDate() + ", " + monthNameShort + "  " + date.getFullYear();
-        return formatted_date;
-    }
-
-var hola = document.getElementById("holaputa").innerHTML = formatDate(date)
+formatDate = (date)=>{
+    var monthNameShort = date.toLocaleString("en-US", { month: "short" });
+    const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    var formatted_date = weekday[date.getDay()] + " " + + date.getDate() + ", " + monthNameShort + "  " + date.getFullYear() + "---" + + date.getHours() + ":" + + date.getMinutes();
+    return formatted_date;
+}
+document.getElementById("dinamic-date").innerHTML = formatDate(date)
