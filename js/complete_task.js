1995c1bdf042845fcee96f4bcdaaf8d0ddd07829
melvinjim <melvinjimenezang@hotmail.com>
2022-04-21 10:20:45 -0500

    the date data on which the activity was marked as completed is saved

--- js/complete_task.js
+++ js/complete_task.js
@@ -1,4 +1,15 @@
 completeTask = function(element){
     checktask = element.parentNode.parentNode;
     checktask.classList.add('hidden-task');
+
+    var date = new Date();
+    var months = ["Jan", "Feb", "Mar","Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+    formatDate = (date)=>{
+        var formatted_date = date.getDate() + " " + months[date.getMonth()] + " " + date.getFullYear();
+        return formatted_date;
+    }
+    
+    var completeDate  = element.parentNode.nextElementSibling.nextElementSibling;
+    completeDate.innerHTML = formatDate(date)
 }
