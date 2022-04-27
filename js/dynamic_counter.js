cededf56230d256ceaa33abaf92ac37dbaf4d276
melvinjim <melvinjimenezang@hotmail.com>
2022-04-27 08:30:26 -0500

    ocultar tareas terminadas y mostrar tareas sin terminar finalizado

--- js/dynamic_counter.js
+++ js/dynamic_counter.js
@@ -45,7 +45,9 @@ var date = new Date();
 formatDate = (date)=>{
     var monthNameShort = date.toLocaleString("en-US", { month: "short" });
     const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-    var formatted_date = weekday[date.getDay()] + " " + + date.getDate() + ", " + monthNameShort + "  " + date.getFullYear() + "-" + + date.getHours() + ":" + + date.getMinutes();
+    var minutos = date.getMinutes()
+    if(minutos < 10) { minutos = '0' + minutos; }
+    var formatted_date = weekday[date.getDay()] + " " + + date.getDate() + ", " + monthNameShort + "  " + date.getFullYear() + "-" + + date.getHours() + ":" + minutos;
     return formatted_date;
 }
 
