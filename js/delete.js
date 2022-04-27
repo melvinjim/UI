04425196140e7fc6b2e81e279a3501eb828b9099
melvinjim <melvinjimenezang@hotmail.com>
2022-04-27 10:59:33 -0500

    filter by completed tasks finished final details

--- js/delete.js
+++ js/delete.js
@@ -1,6 +1,16 @@
+var counter = document.getElementById("tbody-content");
+var countersIncomplete = counter.getElementsByClassName("datos");
+for (x=0;x<countersIncomplete.length;x++){
+    taskIncompleted = countersIncomplete.length 
+    document.getElementById("task-incompleted").innerText = taskIncompleted + " " +  "Incompleted Tasks";
+}
+
 deleteBtn = function(element){
     document.getElementById('deleteLineRow').onclick = () => {
         deleteLineBtn = element.parentElement.parentNode.parentNode;
         deleteLineBtn.parentNode.removeChild(deleteLineBtn);
+        
+        taskIncompleted--
+        document.getElementById("task-incompleted").innerText = taskIncompleted + " " +  "Incompleted Tasks";
     }
 }
